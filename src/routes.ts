0031d0ad9ab679018330f440f3332bce3531f1a0
menna hassan <you@example.commennahossamelden58@gmail.com>
2024-12-20 16:49:33 +0200

    second commit

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { DetailsComponent } from './components/details/details.component';
 import { DonateComponent } from './components/donate/donate.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Home كصفحة افتراضية
@@ -13,6 +15,8 @@ export const routes: Routes = [
   {path: 'shop', component:ShopComponent},
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
   { path: 'donate', component: DonateComponent },
   {path: 'details', component:DetailsComponent},
   {path: '**', component:NotFoundComponent}
